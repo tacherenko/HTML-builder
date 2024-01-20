90a35e163adbfa29ac4500b6bafa2a13fc8b0be0
Tanya <120472619+tacherenko@users.noreply.github.com>
2024-01-20 19:38:54 +0100

    feat: add solution to 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,32 @@
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+const stylesFolderPath = path.join(__dirname, 'styles');
+const distFolderPath = path.join(__dirname, 'project-dist');
+const outputFilePath = path.join(distFolderPath, 'bundle.css');
+
+const compileStyles = async () => {
+    try {
+        // Чтение файлов стилей
+        const files = await fsPromises.readdir(stylesFolderPath);
+        const cssFiles = files.filter(file => path.extname(file) === '.css');
+
+        // Чтение содержимого файлов стилей
+        const cssContents = await Promise.all(
+            cssFiles.map(file => fsPromises.readFile(path.join(stylesFolderPath, file), 'utf-8'))
+        );
+
+        // Конкатенация стилей
+        const bundleContent = cssContents.join('\n');
+
+        // Создание папки 'project-dist' и запись результирующего файла
+        await fsPromises.mkdir(distFolderPath, { recursive: true });
+        await fsPromises.writeFile(outputFilePath, bundleContent, 'utf-8');
+
+        console.log(`Styles compiled successfully into ${outputFilePath}`);
+    } catch (error) {
+        console.error(`Error compiling styles: ${error.message}`);
+    }
+};
+
+compileStyles();
